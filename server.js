8786408e75d57ab294a7f60a2a3eb923f6ece065
dersh0w <anderson.carneiro3301@gmail.com>
2024-08-09 15:09:32 -0300

    feat: conexão com o MongoDB pela URI

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 require("dotenv").config();
+const mongoose = require("mongoose");
 const app = require("./src/app");
 
+const DB_URI = process.env.DB_URI;
+mongoose
+  .connect(DB_URI)
+  .then(() => {
+    console.log(`Connected to MongoDB!`);
+  })
+  .catch((err) => {
+    console.log(`Error connecting to MongoDB!`);
+    console.log(err);
+    process.exit(1);
+  });
+  
 const port = process.env.APP_PORT ?? 3000;
 app.listen(port, () => {
   console.log(`Listening on :${port}`);
+});
+
+process.on("SIGINT", async () => {
+  try {
+    await mongoose.connection.close();
+    console.log("MongoDB connection closed!");
+  } catch (err) {
+    console.log(`Erro closing MongoDB connection: ${err}`);
+  }
+  process.exit(0);
 });
